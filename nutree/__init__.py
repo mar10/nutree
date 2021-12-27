7febf74aa551b2eb4b945c0dac36dcd7aa671aac
Martin Wendt <github@wwwendt.de>
2021-12-27 11:39:34 +0100

    Bump prerelease (0.0.1-a3) [ci skip]

--- nutree/__init__.py
+++ nutree/__init__.py
@@ -16,7 +16,7 @@ NOTE:
     "3.0.0a4" seems not to work in this case!
 """
 # flake8: noqa
-__version__ = "0.0.1-a2"
+__version__ = "0.0.1-a3"
 
 from .node import AmbigousMatchError, IterMethod, Node, TreeError, UniqueConstraintError
 from .tree import Tree
